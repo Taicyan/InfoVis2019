a34e4169f2e2a35b67d48b580ee93394a825ca3b
太地　正和 <199x108x@lib-shizen-a-019.epc.center.kobe-u.ac.jp>
2019-04-10 19:01:38 +0900

    4/10

---
+++ W02/vec3.js
@@ -0,0 +1,77 @@
+Vec3 = function( x, y, z )
+{
+this.x = x;
+this.y = y;
+this.z = z;
+}
+Vec3.prototype.xget = function()
+{
+	return this.x;
+}
+
+Vec3.prototype.yget = function()
+{
+	return this.y;
+}
+
+Vec3.prototype.zget = function()
+{
+	return this.z;
+}
+
+
+Vec3.prototype.add = function( v )
+{
+this.x += v.x;
+this.y += v.y;
+this.z += v.z;
+return this;
+}
+// Sum method
+Vec3.prototype.sum = function()
+{
+return this.x + this.y + this.z;
+}
+//max function
+Vec3.prototype.max =function()
+{
+	if(v.x > v.y && v.x > v.z){
+	return v.x;
+	}
+	if(v.y > v.x && v.y > v.z){
+	return v.y;
+	}
+	if(v.z > v.x && v.z > v.y){
+	return v.z;
+	}
+}
+//mid function
+Vec3.prototype.mid =function()
+{
+	if((v.x > v.y && v.x < v.z) || (v.x < v.y && v.x > v.z)){
+	return v.x;
+	}
+	if((v.y > v.x && v.y < v.z) || (v.y < v.x && v.y > v.z)){
+	return v.y;
+	}
+	if((v.z > v.x && v.z < v.y) || (v.z < v.x && v.z > v.y)){
+	return v.z;
+	}
+}
+//min function
+Vec3.prototype.min =function()
+{
+	if(v.x < v.y && v.x < v.z){
+	return v.x;
+	}
+	if(v.y < v.x && v.y < v.z){
+	return v.y;
+	}
+	if(v.z < v.x && v.z < v.y){
+	return v.z;
+	}
+}
+
+
+
+	
